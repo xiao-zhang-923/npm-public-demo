861684b1b1e5e42ac7813984fd40fd33dd5d9f1b
zhang <1@qq.com>
2023-08-02 15:45:03 +0800

    n

--- main.ts
+++
@@ -1,6 +0,0 @@
-import { createApp } from "vue";
-import "./style.css";
-import App from "./App.vue";
-import Vue3Step from "zvue3-step";
-import 'zvue3-step/style.css'
-createApp(App).use(Vue3Step).mount("#app");

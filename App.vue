702df352c1cf1f47f0fd0d0ad5dbbac41926e501
zhang <821949267@qq.com>
2023-08-02 15:42:12 +0800

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+// import Vue3Step from "./components/Step.vue";
+const stepList = [
+  { title: "验证手机号" },
+  { title: "填写账号信息" },
+  { title: "注册成功" },
+]
+const step = 1;
+</script>
+
+<template>
+  <Vue3Step :step="step" :list="stepList"></Vue3Step>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ main.ts
@@ -0,0 +1,6 @@
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import Vue3Step from "zvue3-step";
+import 'zvue3-step/style.css'
+createApp(App).use(Vue3Step).mount("#app");
